454e838ead1b66e9dbb7ed57a60e2b7464de93e4
Mohammad Azzam Abdulmute Ihraiz <mohammad.a.ihraiz@gmail.com>
2024-04-30 16:28:05 +0300

    First And Last Position using Binary Search twice.

--- FirstAndLastPosition.py
+++ FirstAndLastPosition.py
@@ -1,32 +1,35 @@
 from typing import List
 
 
-def findTargetIndeciesBS(nums: List[int], target: int) -> List[int]:
+def binarySearch(nums: List[int], target: int, lookLeft: bool) -> int:
     left, right = 0, len(nums) - 1
+    ans = -1
     while left <= right:
         mid = (left + right) // 2
-        if nums[mid] == target:
-            left, right = mid, mid
-            while left >= 0 and right < len(nums) and nums[left] == target and nums[right] == target :
-                left, right = left - 1, right + 1
-
-            while left >= 0 and nums[left] == target:
-                left -= 1
-            while right < len(nums) and nums[right] == target:
-                right += 1
-            left, right = left + 1, right - 1
-            if left == right:
-                return [-1, -1]
-            return [left, right]
-
-        elif nums[mid] < target:
+        if target < nums[mid]:
+            right = mid - 1
+        elif target > nums[mid]:
             left = mid + 1
         else:
-            right = mid - 1
-    return [-1, -1]
+            ans = mid
+            if lookLeft:
+                right = mid - 1
+            else:
+                left = mid + 1
+    return ans
 
 
-input1 = [1, 1, 3, 4, 5, 7, 7]
-input2 = 10
+def findTargetIndeciesBS(nums: List[int], target: int) -> List[int]:
+    firstIndex = binarySearch(nums, target, True)
+    if firstIndex == -1:
+        return [-1, -1]
+    lastIndex = binarySearch(nums, target, False)
+    if lastIndex == firstIndex:
+        return [-1, -1]
+    return [firstIndex, lastIndex]
+
+
+input1 = [1, 1, 3, 4, 5, 6, 7]
+input2 = 7
 
 print(findTargetIndeciesBS(input1, input2))
